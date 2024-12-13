73cbf4bb2585b0212db5e9202f6c8beca35b6dc4
minured <minured@qq.com>
2024-12-13 22:21:54 +0800

    feat: windows requirements

--- tasks/view_new_post.py
+++ tasks/view_new_post.py
@@ -5,9 +5,9 @@ from abilities.operation import Operation
 class ViewNewPost(Operation, Notification):
 
     def run(self):
-        self.switch_to_window('Google Chrome')
-        xy = self.get_xy("./pic/new-post.png")
-        self.moveTo(xy)
-        self.click(xy)
-        self.log('任务完成，发送通知邮件')
-        self.notify('任务完成')
+        self.switch_to_window("Google Chrome")
+        # xy = self.get_xy("./pic/new-post.png")
+        # self.moveTo(xy)
+        # self.click(xy)
+        # self.log('任务完成，发送通知邮件')
+        # self.notify('任务完成')
